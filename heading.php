5af5fc6bfd041f565b2c1a4ffa58fab61af0816a
Tultph39236 <156071277+Tultph39236@users.noreply.github.com>
2024-12-02 14:54:24 +0700

    Add files via upload

--- heading.php
+++ heading.php
@@ -1,21 +1,12 @@
-<!-- Page Header Start -->
-<div class="container-fluid bg-secondary mb-5">
-    <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 200px">
-        <h1 class="font-weight-semi-bold text-uppercase mb-3">Our Shop</h1>
-        <div class="d-inline-flex">
-            <p class="m-0"><a href="index.php">Home</a></p>
-            <p class="m-0 px-2">-</p>
-            <p class="m-0">
-                <?php 
-                    if(isset($category_id)) {
-                        $category = query_one("categories", $category_id);
-                        echo $category['name'];
-                    }else {
-                        echo "Products";
-                    }
-                ?>
-            </p>
+    <!-- Page Header Start -->
+    <div class="container-fluid bg-secondary mb-5">
+        <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 200px">
+            <h1 class="font-weight-semi-bold text-uppercase mb-3">Contact Us</h1>
+            <div class="d-inline-flex">
+                <p class="m-0"><a href="">Home</a></p>
+                <p class="m-0 px-2">-</p>
+                <p class="m-0">Contact</p>
+            </div>
         </div>
     </div>
-</div>
-<!-- Page Header End -->
+    <!-- Page Header End -->
--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<?php
-// include "./pages/products/heading.php";
-
+<?php 
+    // include "./pages/contact/heading.php";
+    include "./pages/contact/main.php";
 ?>
-<!-- Shop Start -->
-<div class="container-fluid pt-5">
-    <div class="row px-xl-5">
-        <?php
-        include "./pages/products/sidebar.php";
-        include "./pages/products/list.php";
-        ?>
-    </div>
-</div>
-<!-- Shop End -->
